cd88947ac903e0986fe9f4be144f3a40b6ff3850
Brett Benyo <bbenyo@gmail.com>
2016-03-26 13:50:31 -0400

    Initial implementation, plumbing and quick incomplete translator

---
+++ translator/cadets_cdm_translator.py
@@ -0,0 +1,126 @@
+#!/usr/bin/python
+
+"""
+Load in trace records in CADETS json format, translate them to CDM format, and write the CDM records to a file
+  Outputs a JSON CDM format and the binary avro format
+  
+"""
+
+import logging
+from logging.config import fileConfig
+import argparse
+import os
+import json
+
+from tc.schema.serialization import Utils
+from tc.schema.serialization.kafka import KafkaAvroGenericSerializer
+
+from avro.io import DatumWriter
+from avro.datafile import DataFileWriter
+
+from translator import CDMTranslator
+
+# Default values, replace or use command line arguments
+TRACE_DIR = "../../trace-data"
+IN_FILE = "git_server.json"
+SCHEMA = "/opt/starc/avro/TCCDMDatum.avsc"
+OUTPUT_DIR = "output"
+LOGGING_CONF = "logging.conf"
+
+def get_arg_parser():
+    parser = argparse.ArgumentParser(description="Translate CADETS json to CDM")
+
+    parser.add_argument("-psf", action="store", type=str,
+            default=SCHEMA,
+            help="Set the producer's schema file.")  
+    parser.add_argument("-v", action="store_true", default=False,
+            help="Turn up verbosity.")
+    parser.add_argument("-tdir", action="store", type=str, default=TRACE_DIR,
+                        help="Directory to load CADETS record files from")
+    parser.add_argument("-odir", action="store", type=str, default=OUTPUT_DIR,
+                        help="Directory to write CDM records files to")
+    parser.add_argument("-f", action="store", type=str, default=IN_FILE,
+                        help="File to translate")
+    parser.add_argument("-lc", action="store", type=str, default=LOGGING_CONF,
+                        help="Logging configuration file")
+    parser.add_argument("-wj", action="store_true", default=True, help="Write JSON output file")
+    parser.add_argument("-wb", action="store_true", default=True, help="Write binary output file")
+        
+    return parser
+
+
+def main():
+    parser = get_arg_parser()
+    args = parser.parse_args()
+
+    # Set up logging
+    fileConfig(args.lc)
+    
+    if args.v:
+        logging.getLogger("tc").setLevel(logging.DEBUG)
+   
+    logger = logging.getLogger("tc")
+    
+    # Load the avro schema
+    p_schema = Utils.load_schema(args.psf)
+    
+    # Initialize an avro serializer, this will be used to write out the CDM records
+    serializer = KafkaAvroGenericSerializer(p_schema)
+    
+    # Initialize a CDM Translator
+    translator = CDMTranslator(p_schema)
+    
+    # Load the input file
+    path = os.path.join(args.tdir, args.f)    
+    
+    # Read the JSON CADETS records
+    with open(path, 'r') as cadets_in:
+        logger.info("Loading records from "+cadets_in.name)
+        cadets_records = json.load(cadets_in)
+        cadets_in.close()
+        
+    # Open the output files
+    base_out = os.path.basename(path)
+    json_out = None
+    bin_out = None
+    if args.wj:
+        json_out_path = os.path.join(args.odir, base_out+".CDM.json")
+        json_out = open(json_out_path, 'w')
+    if args.wb:
+        bin_out_path = os.path.join(args.odir, base_out+".CDM.bin")
+        bin_out = open(bin_out_path, 'w')
+        # Create a file writer and serialize all provided records to it.
+        datum_writer = DatumWriter(p_schema)
+        file_writer = DataFileWriter(bin_out, datum_writer, p_schema)
+
+    # Iterate through the records, translating each to a CDM record
+    count = 0
+    for cadets_record in cadets_records:
+        logger.debug("{i} Record: {data}".format(i=count, data=cadets_record))
+        cdm_records = translator.translate_record(cadets_record)
+        
+        for cdm_record in cdm_records:
+            if cdm_record != None:
+                logger.debug("{i} Translated record: {data}".format(i=count, data=cdm_record))
+                count += 1
+        
+                if args.wj:
+                    # Serialize the record
+                    jout = serializer.serialize_to_json(cdm_record)
+                    json_out.write(jout+"\n")
+        
+                if args.wb:
+                    file_writer.append(cdm_record)
+            
+        
+    logger.info("Translated {i} records".format(i=count))
+   
+    if json_out != None:
+        json_out.close()
+        logger.info("Wrote JSON CDM records to {jo}".format(jo=json_out.name))
+         
+    if bin_out != None:
+        bin_out.close()
+        logger.info("Wrote binary CDM records to {bo}".format(bo=bin_out.name))
+        
+main()
---
+++ translator/translator.py
@@ -0,0 +1,222 @@
+'''
+CADETS JSON record format to TC CDM format translator
+'''
+
+import logging
+
+class CDMTranslator(object):
+    
+    # Processes that we created Process subjects for mapped to the uid of the Process Subject
+    created_processes = {}
+   
+    # Threads that we created Subjects for mapped to the uid of the Thread subject
+    created_threads = {}
+    
+    # Users that we created a Principal for mapped to the uid of the Principal
+    created_users = {}
+
+    # True: Raise an exception if we can't translate a record, False: just log it
+    exceptionOnError = True
+
+    # Event counter for unique ids
+    eventCounter = 0
+
+    def __init__(self, schema):
+        self.schema = schema
+        self.logger = logging.getLogger("tc")
+        
+    def get_source(self):
+        ''' Get the InstrumentationSource, for now this is hardcoded to SOURCE_FREEBSD_DTRACE_CADETS '''
+        return "SOURCE_FREEBSD_DTRACE_CADETS"
+        # return "SOURCE_FREEBSD_OPENBSM_TRACE"
+    
+    def create_uuid(self, object_type, data):
+        ''' Create a unique ID from an object type and data value
+            For now, we use the data as the lower 32 bits
+            For "uid", we set the next byte to 0x0
+            For "pid", we set the next byte to 0x1
+            For "tid", we set the next byte to 0x2
+            For events, 0x3
+            0    16   32   48     (for a pid, where data is the pid value)
+            0000 0001 data data   
+        '''
+        uuid = 0
+        if object_type == "pid":
+            uuid = (1 << 32)
+        elif object_type == "tid":
+            uuid = (2 << 32)
+        elif object_type == "event":
+            uuid = (3 << 32)
+        uuid = uuid | data
+        return uuid
+        
+    def get_process_subject_id(self, pid):
+        ''' Given a pid, did we create a subject for the pid previously? 
+            If som return the uid of the subject, if not reutrn None
+        '''
+        if self.created_processes.has_key(pid):
+            return self.created_processes[pid]
+        
+        return None
+    
+    def create_process_subject(self, pid, time_micros, source):
+        ''' Create a process subject, add it to the created list, and return the dict '''
+        record = {}
+        subject = {}
+        subject["properties"] = {}
+        
+        subject["pid"] = pid
+        subject["ppid"] = -1  # TODO: Default value?
+        subject["startTimestampMicros"] = time_micros
+        subject["source"] = source
+        subject["type"] = "SUBJECT_PROCESS"
+        
+        # Generate a uuid for this subject
+        uniq = self.create_uuid("pid", pid)
+        self.created_processes[pid] = uniq
+        subject["uuid"] = uniq
+        
+        record["datum"] = subject
+        return record
+        
+    def get_thread_subject_id(self, tid):
+        ''' Given a tid, did we create a subject for tid?
+            If so, return the uid of the subject, if not return None
+        '''
+        if self.created_threads.has_key(tid):
+            return self.created_threads[tid]
+        
+        return None
+    
+    def create_thread_subject(self, tid, time_micros, source):
+        ''' Create a thread subject, add it to the created list, and return the dict '''
+        record = {}
+        subject = {}
+        subject["properties"] = {}
+        
+        subject["startTimestampMicros"] = time_micros
+        subject["source"] = source
+        subject["type"] = "SUBJECT_THREAD"
+        subject["pid"] = tid # TODO: Do we put the tid here in the pid field?
+        subject["ppid"] = -1 # TODO: This should be optional
+        
+        # Generate a uuid for this subject
+        uniq = self.create_uuid("tid", tid)
+        self.created_threads[tid] = uniq
+        subject["uuid"] = uniq
+        
+        # TODO: tid can be a property
+        # subject["properties"]["tid"] = tid
+        
+        record["datum"] = subject
+        return record
+    
+    def get_user_id(self, uid):
+        ''' Given a uid, did we create a Principal for that uid?
+            If so, return the uid of the Principal, if not return None
+        '''
+        if self.created_users.has_key(uid):
+            return self.created_users[uid]
+        
+        return None
+    
+    def create_user_principal(self, uid, source):
+        ''' Create a user principal, add it to the created list, and return the dict '''
+        record = {}
+        principal = {}
+        principal["properties"] = {}
+        principal["userId"] = uid
+        principal["source"] = source
+        principal["groupIds"] = []
+        principal["type"] = "PRINCIPAL_LOCAL"
+        
+        # Generate a uuid for this user
+        uniq = self.create_uuid("uid", uid)
+        self.created_users[uid] = uniq
+        principal["uuid"] = uniq
+                
+        record["datum"] = principal
+        return record
+    
+    def handle_error(self, msg):
+        if self.exceptionOnError:
+            raise msg
+        else:
+            self.logger.error(msg)
+
+    def translate_record(self, cadets_record):
+        ''' Generate a CDM record from the passed in JSON CADETS record '''
+        
+        # List of CDM vertices or edges that we created from this record
+        datums = []
+        
+        time_micros = cadets_record["time"]
+        
+        # Create a new user if necessary
+        uid = cadets_record["uid"]
+        user_uuid = self.get_user_id(uid)
+        if user_uuid == None:
+            self.logger.info("Creating new User Principal for {u}".format(u=uid))
+            principal = self.create_user_principal(uid, self.get_source())
+            datums.append(principal)
+            
+        # Create a new Process subject if necessary
+        pid = cadets_record["pid"]
+        proc_uuid = self.get_process_subject_id(pid)
+        if proc_uuid == None:
+            self.logger.info("Creating new Process Subject for {p}".format(p=pid))
+            process = self.create_process_subject(pid, time_micros, self.get_source())
+            datums.append(process)
+            
+        # Create a new Thread subject if necessary
+        tid = cadets_record["tid"]
+        thread_uuid = self.get_thread_subject_id(tid)
+        if thread_uuid == None:
+            self.logger.info("Creating new Thread Subject for {t}".format(t=tid))
+            thread = self.create_thread_subject(tid, time_micros, self.get_source())
+            datums.append(thread)
+        
+        # dispatch based on type
+        event_type = cadets_record["event"]
+        event_components = event_type.split(":")
+        if len(event_components) < 4:            
+            self.handle_error("Expecting 4 elements in the event type: provider:module:function:probe. Got: "+event_type)
+                
+        module = event_components[1]
+        call = event_components[2]
+        probe = event_components[3]
+                
+        if event_components[0] == "syscall":
+            datums.append(self.translate_syscall(module, call, probe, cadets_record))
+        else:
+            datums.append(self.translate_appcall(event_components[0], module, call, probe, cadets_record))
+                    
+        return datums
+    
+    def translate_syscall(self, module, call, probe, cadets_record):
+        ''' Translate a syscall event '''
+        
+        # for now, ignore return probe, only translate entry
+        if probe == "return":
+            self.logger.warn("Ignoring return probe")
+            return None
+        
+        record = {}
+        event = {}
+        event["properties"] = {}
+
+        uuid = self.create_uuid("event", self.eventCounter)
+        self.eventCounter += 1
+        
+        event["uuid"] = uuid
+        # TODO: Translate the event type, for now map everything to OS_UNKNOWN
+        event["type"] = "EVENT_OS_UNKNOWN"
+        event["threadId"] = cadets_record["tid"]
+        event["timestampMicros"] = cadets_record["time"]
+        event["sequence"] = 0 # TODO: Sequence numbers
+        event["source"] = self.get_source()
+        # TODO: arguments
+        
+        # TODO: Add event edge
+        record["datum"] = event
+        return record
