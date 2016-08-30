be6e7f49bc775ecb876581545551c4902837b30f
Arun Thomas <arun.thomas@baesystems.com>
2016-08-29 22:30:48 -0400

    audit.sh: Run dtrace in a loop

--- translator/audit.sh
+++ translator/audit.sh
@@ -2,5 +2,9 @@
 
 set -e -x
 
-FILE=/data/$(date +%Y%m%d%H%M%S).json
-sudo /opt/starc/dtrace-scripts/audit.d > ${FILE}
+#sudo rm -i /data/2016*.json
+
+while true; do
+	FILE=/data/$(date +%Y%m%d%H%M%S).json
+	sudo /opt/starc/dtrace-scripts/audit.d > ${FILE}
+done
