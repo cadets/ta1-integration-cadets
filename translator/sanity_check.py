910ad4f1cd06a6c69b50eaeddba1a8fc99cad95a
Amanda Strnad <amanda.strnad@baesystems.com>
2016-09-01 15:53:09 -0400

    sanity_checker: Comment + better output

--- translator/sanity_check.py
+++ translator/sanity_check.py
@@ -12,11 +12,6 @@ import argparse
 import os
 import json
 
-from tc.schema.serialization import Utils
-from tc.schema.serialization.kafka import KafkaAvroGenericDeserializer
-
-from translator import CDMTranslator
-
 # Default values, replace or use command line arguments
 SCHEMA = "../../ta3-serialization-schema/avro/TCCDMDatum.avsc"
 LOGGING_CONF = "logging.conf"
@@ -25,17 +20,12 @@ CDMVERSION = "13"
 logger = logging.getLogger("tc")
 
 def get_arg_parser():
-    parser = argparse.ArgumentParser(description="Translate CADETS json to CDM")
+    parser = argparse.ArgumentParser(description="Examine CDM json produced by CADETS")
 
-    parser.add_argument("-psf", action="store", type=str,
-                        default=SCHEMA,
-                        help="Set the producer's schema file.")
     parser.add_argument("-v", action="store_true", default=False,
                         help="Turn up verbosity.")
     parser.add_argument("-lc", action="store", type=str, default=LOGGING_CONF,
                         help="Logging configuration file")
-    parser.add_argument("-cdmv", action="store", type=str, default=CDMVERSION,
-                        help="CDM Version number, make sure this matches the schema file you set with psf")
     parser.add_argument("files", nargs="+", help="files to check")
 
     return parser
@@ -51,38 +41,29 @@ def main():
     if args.v:
         logging.getLogger("tc").setLevel(logging.DEBUG)
 
-    # Load the avro schema
-    p_schema = Utils.load_schema(args.psf)
-
-    # Initialize a CDM Translator
-    translator = CDMTranslator(p_schema, args.cdmv)
-
     for cfile in args.files:
     # Load the input file
         path = os.path.expanduser(cfile)
-        examine_file(translator, path)
+        examine_file(path)
 
 referenced_uuids = {}
 
-def examine_file(translator, path):
+def examine_file(path):
     '''
     Go through file line-by-line to check each record and look for any warning signs
     '''
-    logger.info("Examining JSON file: "+path)
-    p_schema = translator.schema
-    # Initialize an avro serializer, this will be used to write out the CDM records
-    deserializer = KafkaAvroGenericDeserializer(p_schema, p_schema)
 
     # Read the JSON CADETS records
+    logger.info("Examining JSON file: "+path)
     with open(path, 'r') as cadets_in:
         referenced_uuids.clear()
         incount = 0
         # Iterate through the records, translating each to a CDM record
         while 1:
             raw_record = cadets_in.readline()
-            if not raw_record:
+            if not raw_record: # EOF
                 break
-            if raw_record == "\n":
+            if raw_record == "\n": # blank line
                 continue
 
             incount = incount + 1
@@ -90,18 +71,18 @@ def examine_file(translator, path):
             try:
                 cadets_record = json.loads(raw_record)
             except ValueError:
-                logger.warn("Invalid CDM entry: " + raw_record)
+                logger.error("Invalid CDM entry: " + raw_record)
                 continue
 
-            validated = examine_record(translator, cadets_record)
+            validated = examine_record(cadets_record)
             if not validated:
-                logger.warn("Check record #" + str(incount) + ": " + raw_record)
+                logger.error("Check record #" + str(incount) + ": " + raw_record.strip())
 
         cadets_in.close()
 #         logger.info("referenced_uuids size: " + str(len(referenced_uuids)))
 
 
-def examine_record(translator, record):
+def examine_record(record):
     '''
     Check if there is anything unexpected about the record
 
