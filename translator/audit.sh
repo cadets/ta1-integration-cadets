d7010e4e4122ac483c94bfbbcad9a42f3e9dee51
Arun Thomas <arun.thomas@baesystems.com>
2017-10-12 12:53:31 +0000

    audit.sh: add hostuuid

--- translator/audit.sh
+++ translator/audit.sh
@@ -6,5 +6,5 @@ set -x
 
 while true; do
 	FILE=/data/$(date +%Y%m%d%H%M%S).json
-	sudo /opt/starc/dtrace-scripts/audit.d -o ${FILE}
+	sudo /opt/starc/dtrace-scripts/audit.d -o ${FILE} $(sysctl -n kern.hostuuid)
 done
