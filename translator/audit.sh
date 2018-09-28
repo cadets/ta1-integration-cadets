0a57f3fb02259dc747aa26359bd48d9d6e52c8dd
Amanda Strnad <amanda.strnad@baesystems.com>
2018-09-28 22:44:25 +0000

    Update script to run audit.d

--- translator/audit.sh
+++ translator/audit.sh
@@ -6,5 +6,6 @@ set -x
 
 while true; do
 	FILE=/data/$(date +%Y%m%d%H%M%S).json
-	sudo /opt/starc/dtrace-scripts/audit.d -o ${FILE} $(sysctl -n kern.hostuuid)
+# 	sudo /opt/starc/dtrace-scripts/audit.d -o ${FILE} $(sysctl -n kern.hostuuid)
+	sudo /opt/starc/dtrace-scripts/run_auditd.sh > ${FILE}
 done
