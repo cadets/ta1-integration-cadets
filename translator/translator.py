36feebd2640b91e2cd0b9f6339e9731756336144
Brian Kidney <bkidney@briankidney.ca>
2019-03-02 11:35:18 -0330

    Adds recording of username when available.
    When username is available it will be written into the CDM Principal
    record. Using `cadets_record.get("username") retruns `None` when the
    field does not exist.

--- translator/translator.py
+++ translator/translator.py
@@ -193,7 +193,7 @@ class CDMTranslator(object):
         uid = cadets_record["uid"]
         if not ig.get_user_id(uid):
             self.logger.debug("Creating new User Principal for %d", uid)
-            principal = ig.create_user_principal(uid, cadets_record["host"], self.get_source())
+            principal = ig.create_user_principal(uid, cadets_record["host"], self.get_source(), cadets_record.get("username"))
             datums.append(principal)
 
         # Create a new Process subject if necessary
